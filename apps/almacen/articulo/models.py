dfdcf3c9e2c934b8910e188db53bc1c62d87f7dd
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2017-01-04 21:16:44 -0500

    Se creo los modelos de Categoria y Articulo y se hizo su migracion

--- apps/almacen/articulo/models.py
+++ apps/almacen/articulo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from apps.almacen.categoria.models import Categoria
 # Create your models here.
+class Articulo(models.Model):
+    idarticulo = models.AutoField(primary_key=True)
+    codigo= models.CharField(max_length=50)
+    nombre= models.CharField(max_length=100)
+    stock= models.IntegerField()
+    descripcion=models.CharField(max_length=512)
+    imagen=models.CharField(max_length=50)
+    estado=models.CharField(max_length=20)
+    idcategoria= models.ForeignKey(Categoria,db_column='idcategoria',null=True,blank=True,on_delete=models.CASCADE)
--- apps/almacen/categoria/models.py
+++ apps/almacen/categoria/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre= models.CharField(max_length=50)
+    descripcion= models.CharField(max_length=256)
+    condicion= models.BooleanField(default=1)
