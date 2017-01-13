a6545e9220e4b8a934ad04ab9cf301fbe9b5e5ee
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2017-01-12 19:50:23 -0500

    Se agrego la funcionalidad editar al crud de categorias funciona perfectamente

--- apps/almacen/categoria/urls.py
+++ apps/almacen/categoria/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.conf.urls import url
-from apps.almacen.categoria.views import CategoriaList,CategoriaCreate
+from apps.almacen.categoria.views import CategoriaList,CategoriaCreate,CategoriaUpdate
 
 urlpatterns = [
     url(r'^categoria/$',CategoriaList.as_view(),name='categoria_listar'),
     url(r'^categoria/create$',CategoriaCreate.as_view(),name='categoria_create'),
+    url(r'^categoria/edit/(?P<pk>\d+)$',CategoriaUpdate.as_view(),name='categoria_edit'),
 ]
--- apps/almacen/categoria/views.py
+++ apps/almacen/categoria/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
 from apps.almacen.categoria.models import Categoria
 from apps.almacen.categoria.forms import CategoriaForm
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,UpdateView
 from pure_pagination.mixins import PaginationMixin
 # Create your views here.
 class CategoriaList(PaginationMixin,ListView):
@@ -16,3 +16,9 @@ class CategoriaCreate(CreateView):
     form_class= CategoriaForm
     template_name='almacen/categoria/categoria_form.html'
     success_url= reverse_lazy('almacen:categoria_listar')
+
+class CategoriaUpdate(UpdateView):
+    model=Categoria
+    form_class=CategoriaForm
+    template_name='almacen/categoria/categoria_form.html'
+    success_url = reverse_lazy('almacen:categoria_listar')
