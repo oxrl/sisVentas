dca729bcf0409c299bbf8890520a39efa9a1086c
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2017-01-14 01:03:30 -0500

    Se añadio la funcionalidad Eliminar categoria

--- apps/almacen/categoria/urls.py
+++ apps/almacen/categoria/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.conf.urls import url
-from apps.almacen.categoria.views import CategoriaList,CategoriaCreate,CategoriaUpdate
+from apps.almacen.categoria.views import CategoriaList,CategoriaCreate,CategoriaUpdate,CategoriaDelete
 
 urlpatterns = [
     url(r'^categoria/$',CategoriaList.as_view(),name='categoria_listar'),
     url(r'^categoria/create$',CategoriaCreate.as_view(),name='categoria_create'),
     url(r'^categoria/edit/(?P<pk>\d+)$',CategoriaUpdate.as_view(),name='categoria_edit'),
+    url(r'^categoria/delete/(?P<pk>\d+)$',CategoriaDelete.as_view(),name='categoria_delete'),
 ]
